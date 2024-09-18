85ec7c81349efc8e412e73fadf9a0eee825f1fe8
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-09-18 19:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const inputs = document.querySelectorAll('.code');
+const codes = document.querySelectorAll('.code');
 
-inputs.forEach((input, index) => {
-    input.addEventListener('input', (event) => {
-        if (index < inputs.length - 1 && event.target.value) {
-            inputs[index + 1].focus();
-        }
-    });
+// Function to move focus to the next input
+function moveToNextInput(currentInput, nextInput) {
+  if (currentInput.value.length === 1) {
+    nextInput.focus();
+  }
+}
 
-    input.addEventListener('keyup', (event) => {
-        if (event.key === 'Backspace' && !input.value && index - 1 >= 0) {
-            inputs[index - 1].focus();
-        }
-    });
+// Function to move focus to the previous input
+function moveToPreviousInput(currentInput, previousInput) {
+  if (currentInput.value === '') {
+    previousInput.focus();
+  }
+}
+
+codes.forEach((input, index) => {
+  // Event for typing in the input
+  input.addEventListener('input', () => {
+    if (input.value.length === 1 && index < codes.length - 1) {
+      moveToNextInput(input, codes[index + 1]);
+    }
+  });
+
+  // Event for handling backspace
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (index > 0) {
+        moveToPreviousInput(input, codes[index - 1]);
+      }
+    }
+  });
+
+  // Set focus on the first input when the page loads
+  if (index === 0) {
+    input.focus();
+  }
 });
